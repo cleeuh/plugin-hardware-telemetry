df1da90461779a244cae8432507f8f431e93b8ac
root <clee48@hawaii.edu>
2024-01-11 22:25:23 +0000

    added more options to test

--- run_on_node_dev.sh
+++ run_on_node_dev.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # --develop allows access to wan network
+# privileged gives access to more devices
 git fetch && git pull
 sudo pluginctl run --name plugin-network-test --privileged --develop $(sudo pluginctl build .)
