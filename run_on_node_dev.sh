8b08ff227f62c47eb69827a703a98488c78fc2b8
root <clee48@hawaii.edu>
2024-01-11 22:19:23 +0000

    added privileged

--- run_on_node_dev.sh
+++ run_on_node_dev.sh
@@ -2,4 +2,4 @@
 
 # --develop allows access to wan network
 git fetch && git pull
-sudo pluginctl run --name plugin-network-test --develop $(sudo pluginctl build .)
+sudo pluginctl run --name plugin-network-test --privileged --develop $(sudo pluginctl build .)
